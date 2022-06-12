9d0fab714b655fafc86956f4cd2f29c88093db6f
Keiji Suzuki <zuki.ebetsu@gmail.com>
2022-06-12 15:53:06 +0900

    mac用に変更

--- config.mk
+++ config.mk
@@ -1,7 +1,8 @@
 RASPI := 3
 
 ARCH := aarch64
-CROSS := aarch64-linux-gnu-
+#CROSS := aarch64-linux-gnu-
+CROSS := aarch64-unknown-linux-gnu-
 CC := $(CROSS)gcc
 LD := $(CROSS)ld
 OBJDUMP := $(CROSS)objdump
